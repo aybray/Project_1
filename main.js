4b64c4372248eb0b60879fdbdb659f54801c07f4
Eileen Tomy <eileentomy@gmail.com>
2024-10-05 11:40:11 -0400

    test

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // Javascript code to be inserted
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();

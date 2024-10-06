6ba78a30bcd284e4f7a4bb265031d26fc4117986
Eileen Tomy <eileentomy@gmail.com>
2024-10-06 02:41:43 -0400

    move function frm second.html to js

--- second-page.js
+++ second-page.js
@@ -1,1 +1,9 @@
 // Javascript code for second.html
+window.onload = function() {
+    // Get the value from localStorage
+    var mylat = localStorage.getItem('mylat');
+    var mylng = localStorage.getItem('mylng');
+    // Display the value on the page
+    var output = document.getElementById('value-output');
+    output.innerText = mylat;
+    };
